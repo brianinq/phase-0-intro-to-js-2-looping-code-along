9bac6f3daa1b01e1aabdccc283233d0a69651800
brian <kipkoechbrian0@gmail.com>
2022-08-03 17:05:37 +0300

    Complete assignment requirements

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, name){
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        const element = names[i];
+        let msg = `Thank you, ${element}, for the wonderful ${name} gift!`
+        messages.push(msg)
+    }
+    return messages
+}
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        --number
+    }
+}
